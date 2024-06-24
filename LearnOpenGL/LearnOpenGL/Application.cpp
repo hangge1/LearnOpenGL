e3a7afd8ffdbea3a16525b231cbf0b933435f239
hangdada <824362330@qq.com>
2024-06-24 23:39:56 +0800

    [feature] 1、利用EBO和不利用EBO绘制矩形

--- LearnOpenGL/LearnOpenGL/Application.cpp
+++ LearnOpenGL/LearnOpenGL/Application.cpp
@@ -14,36 +14,8 @@ void processInput(GLFWwindow* window)
         glfwSetWindowShouldClose(window, true);
 }
 
-int main()
+int InitShader()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -2;
-    }
-
-    //实际上也可将视口设置为比GLFW的窗口小
-    //本质：屏幕空间变换
-    glViewport(0, 0, 800, 600);
-
-    //当用户改变窗口的大小的时候，视口也应该被调整
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-
     //编译、链接生成着色器
     const char* vertexShaderSource = "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
@@ -89,7 +61,7 @@ int main()
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) 
+    if (!success)
     {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
         std::cout << "ERROR::PROGRAM\n" << infoLog << std::endl;
@@ -100,11 +72,84 @@ int main()
     glDeleteShader(fragmentShader);
     //------------------------------------
 
-    float vertices[] =
+    return shaderProgram;
+}
+
+GLFWwindow* InitWindow(int width, int height, const char* title)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+    if (window == NULL)
     {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return nullptr;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return nullptr;
+    }
+
+    //实际上也可将视口设置为比GLFW的窗口小
+    //本质：屏幕空间变换
+    glViewport(0, 0, width, height);
+
+    //当用户改变窗口的大小的时候，视口也应该被调整
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    return window;
+}
+
+int main()
+{
+    GLFWwindow* window = InitWindow(800, 600, "Hello World");
+    if (window == nullptr)
+    {
+        std::cout << "InitWindow Error!" << std::endl;
+        return -1;
+    }
+
+    int shaderProgram = InitShader();
+    if (shaderProgram < 0)
+    {
+        std::cout << "InitShader Error!" << std::endl;
+        return -2;
+    }
+
+    //不用EBO的VBO存储顶点数据
+    //float vertices[] = 
+    //{
+    //    // 第一个三角形
+    //    0.5f, 0.5f, 0.0f,   // 右上角
+    //    0.5f, -0.5f, 0.0f,  // 右下角
+    //    -0.5f, 0.5f, 0.0f,  // 左上角
+    //    // 第二个三角形
+    //    0.5f, -0.5f, 0.0f,  // 右下角
+    //    -0.5f, -0.5f, 0.0f, // 左下角
+    //    -0.5f, 0.5f, 0.0f   // 左上角
+    //};
+
+
+
+    float vertices[] = 
+    {
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
+    };
+
+    unsigned int indices[] = 
+    {
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
     };
 
     //生成VAO
@@ -117,6 +162,12 @@ int main()
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //生成IBO
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     //解释顶点属性
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -134,10 +185,11 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        //不用EBO的DrawCall
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-
+        //用EBO的DrawCall
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 
 
