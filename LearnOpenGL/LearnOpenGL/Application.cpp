db16d8d635e43cfca7410e2ba809fe1aed0026ba
hangdada <824362330@qq.com>
2024-06-26 16:25:03 +0800

    [feature] 1、添加assimp库配置相关内容 2、添加models资产文件夹，添加一个模型资产

--- LearnOpenGL/LearnOpenGL/Application.cpp
+++ LearnOpenGL/LearnOpenGL/Application.cpp
@@ -17,6 +17,10 @@
 
 #include "Camera.h"
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 float deltaTime = 0.0f; // 当前帧与上一帧的时间差
 float lastFrame = 0.0f; // 上一帧的时间
 
@@ -97,8 +101,27 @@ GLFWwindow* InitWindow(int width, int height, const char* title)
     return window;
 }
 
+//void loadModel(const std::string& path)
+//{
+//    Assimp::Importer import;
+//    const aiScene* scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+//
+//    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+//    {
+//        std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
+//        return;
+//    }
+//    //directory = path.substr(0, path.find_last_of('/'));
+//
+//    //processNode(scene->mRootNode, scene);
+//}
+
 int main()
 {
+    //loadModel("assets/models/nanosuit/nanosuit.obj");
+    //return 0;
+
+
     int screenWidth = 800;
     int screenHeight = 600;
 
