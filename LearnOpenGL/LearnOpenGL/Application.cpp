0fdd0d0f5bf1fa9b370a194422f50ddc4e36d2e2
hangdada <824362330@qq.com>
2024-06-27 15:41:17 +0800

    [feature] 1、学习并测试模板缓冲相关API 2、实现物体轮廓显示功能 TODO：和LearnOpenGL不一样，自己思考的！但是仍然存在bug： 地板下面看，会出问题

--- LearnOpenGL/LearnOpenGL/Application.cpp
+++ LearnOpenGL/LearnOpenGL/Application.cpp
@@ -112,33 +112,181 @@ int main()
         return -1;
     }
 
+    //启动深度测试
     glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    //启动模板测试
+    glEnable(GL_STENCIL_TEST);
 
-    Shader ourShader("assets/shader/modelShader.vs", "assets/shader/modelShader.fs");
-    Model model_nanosuit("assets/models/nanosuit/nanosuit.obj");
-    ourShader.Bind();
+    float cubeVertices[] = 
+    {
+        // positions          // texture Coords
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+
+    float planeVertices[] = 
+    {
+        // positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
+         5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+        -5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
+        -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+
+         5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+        -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+         5.0f, -0.5f, -5.0f,  2.0f, 2.0f
+    };
+
+    // cube VAO
+    unsigned int cubeVAO, cubeVBO;
+    glGenVertexArrays(1, &cubeVAO);
+    glGenBuffers(1, &cubeVBO);
+    glBindVertexArray(cubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glBindVertexArray(0);
+    // plane VAO
+    unsigned int planeVAO, planeVBO;
+    glGenVertexArrays(1, &planeVAO);
+    glGenBuffers(1, &planeVBO);
+    glBindVertexArray(planeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glBindVertexArray(0);
+
+    Texture cubeTexture("assets/textures/diffuseMap.png");
+    Texture floorTexture("assets/textures/wall.jpg");
+    cubeTexture.Bind(0);
+    floorTexture.Bind(1);
+
+    Shader shader("assets/shader/stencil_testing.vs", "assets/shader/stencil_testing.fs");
+    Shader shaderSingleColor("assets/shader/stencil_testing.vs", "assets/shader/stencil_single_color.fs");
 
     while (!glfwWindowShouldClose(window)) //渲染循环
     {
         processInput(window);
 
         //Clear
-        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClearStencil(0);
+        glClearDepth(1.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-        glm::mat4 projection = glm::perspective(glm::radians(camera.GetFov()), (float)screenWidth / screenHeight, 0.1f, 100.0f);
-        glm::mat4 view = camera.GetViewMatrix();
-        ourShader.SetUniform4mat("projection", projection);
-        ourShader.SetUniform4mat("view", view);
-
-        // render the loaded model
+        //======================================================Draw
+        shaderSingleColor.Bind();
         glm::mat4 model(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
-        ourShader.SetUniform4mat("model", model);
+        glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(camera.GetFov()), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
+        shaderSingleColor.SetUniform4mat("view", view);
+        shaderSingleColor.SetUniform4mat("projection", projection);
 
-        model_nanosuit.Draw(ourShader);
+        shader.Bind();
+        shader.SetUniform4mat("view", view);
+        shader.SetUniform4mat("projection", projection);
 
+        //========绘制轮廓========
+
+        //1、绘制地板
+        //设置规则: 模板缓冲始终保持不变
+        glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+        glBindVertexArray(planeVAO);
+        floorTexture.Bind();
+        shader.SetUniform4mat("model", glm::mat4(1.0f));
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
+
+        //2、设置模板测试规则： 
+        //模板和深度测试都通过，模板缓冲置1
+        glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+        glStencilFunc(GL_ALWAYS, 1, 0xFF);
+        glStencilMask(0xFF);
+        //绘制立方体
+        glBindVertexArray(cubeVAO);
+        glActiveTexture(GL_TEXTURE0);
+        cubeTexture.Bind();
+        model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+        shader.SetUniform4mat("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+        shader.SetUniform4mat("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        //======>到目前位置，已经将两个立方体对应位置模板缓冲置为1，其余为0
+
+
+        //3、渲染边框
+        //设置模板测试规则： 如果模板缓冲内容 != 1，则通过
+        glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+        glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+        glDisable(GL_DEPTH_TEST); //设置边框置顶绘制
+        shaderSingleColor.Bind();
+        float scale = 1.1f;
+        // cubes
+        glBindVertexArray(cubeVAO);
+        cubeTexture.Bind();
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+        model = glm::scale(model, glm::vec3(scale, scale, scale));
+        shaderSingleColor.SetUniform4mat("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(scale, scale, scale));
+        shaderSingleColor.SetUniform4mat("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+        //glStencilMask(0xFF);
+        //glStencilFunc(GL_ALWAYS, 0, 0xFF);
+        glEnable(GL_DEPTH_TEST);
+
+        //End======================================================Draw
         glfwPollEvents();
         glfwSwapBuffers(window);
 
@@ -147,9 +295,6 @@ int main()
         lastFrame = currentFrame;
     }
 
-    /*glDeleteBuffers(1, &VBO);
-    glDeleteVertexArrays(1, &lightVAO);
-    glDeleteVertexArrays(1, &objectVAO);*/
 
     glfwTerminate();
     return 0;
